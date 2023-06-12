c603061bd8a593a4b3e5304c043fc9fafdbfe6f1
RatifiedGeorge <87443655+Ratified@users.noreply.github.com>
2023-06-12 07:06:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,25 @@
 const errorMessage = document.querySelector('#email__error-message');
 const emailInput = document.querySelector('#email__address');
-var emailSpan = document.getElementById("display-email");
 const submitBtn = document.querySelector('#subscribe');
 
-submitBtn.addEventListener('click', (e) =>{
+submitBtn.addEventListener('click', (e) => {
   e.preventDefault();
 
   const emailAddress = emailInput.value;
-  
+
   const isValidEmail = validateEmail(emailAddress);
 
-  if(isValidEmail){
+  if (isValidEmail) {
     errorMessage.style.display = 'none';
-    emailSpan.value = emailAddress;
+    localStorage.setItem('email', emailAddress); // Store the email address in local storage
     window.location.href = './thanks.html';
-  }
-
-  else{
+  } else {
     errorMessage.style.display = 'block';
     emailInput.style.border = '1px solid red';
   }
-})
+});
 
-function validateEmail(email){
-      const regex = new RegExp(/^[\w.-]+@[\w.-]+\.\w+$/);
-      return regex.test(email);
+function validateEmail(email) {
+  const regex = new RegExp(/^[\w.-]+@[\w.-]+\.\w+$/);
+  return regex.test(email);
 }
-
-
